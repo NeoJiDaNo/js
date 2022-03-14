c42ef673be00ffe72f4b4821256b985660afa990
Dmitriy <gmissing@mail.ru>
2022-03-14 13:31:54 +0300

    js2-hard

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('Hello')
+let num = 266219;
+let arr = num.toString().split("");
+
+let result = arr.reduce(function(sum, current) {
+    return sum * current;
+});
+
+console.log(result);
+
+let obj = (result ** 3);
+
+console.log(obj);
+console.log(obj.toString().substring(0, 2));
+
