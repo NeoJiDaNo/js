27212e7f4cdbc64a83b7abe6380b2ab76958b1f0
Dmitriy <gmissing@mail.ru>
2022-03-14 12:50:32 +0300

    js2

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-alert('Hello')
+let title = "Project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 666666;
+let rollback = 69;
+let fullPrice = 666666666;
+let adaptive = true;
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов" + screenPrice + "рублей" + " " + "Стоимость разработки сайта" + fullPrice + "рублей");
+
+console.log(screens.toLowerCase);
+console.log(screens.split(","));
+
+console.log((fullPrice * (rollback/100)));
