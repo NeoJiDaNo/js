6f0590f9967e22f429cca096b1c8dc1e351a3cf4
Vasily <vasily-bazhenov.03@mail.ru>
2022-03-18 14:24:57 +0300

    js2

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const text = prompt('Текст', 'Необходимо выполнить в отдельном JS файле, подключенному к отдельной HTML странице')
 
 function textOk(a) {
-    if(typeof a === 'string'){
-        console.log('строчка');
+    if(typeof a !== 'string'){
+        console.log('не строчка');
+    } else if(typeof a === 'string' && a.length > 30) {
+        console.log('строчка больше 30');
         a = text.trim()
         console.log(a);
         a = text.slice(0, 30) + '...';
         console.log(a);
-    } else if(typeof a !== 'string') {
-        console.log('не строчка');
+    } 
+    else if(typeof a === 'string') {
+        console.log('строчка');
+        a = text.trim()
+        console.log(a);
     }
 }
 
