107cf1fa1a0b2221ae1433be2dcfd182cf23d008
Vasily <vasily-bazhenov.03@mail.ru>
2022-03-22 15:49:58 +0300

    js

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
-let num = Math.floor(Math.random() * 100);
 alert('Угадай число от 1 до 100');
-console.log(num);
+let num;
+let chel;
 
 
-function one(x) {
-    let y = prompt('Напиши твоё число от 1 до 100', 50)
-    function filter() {
-        if (false == isNaN(y)) {
-            num = Number(y)
-        } else {
-            alert('Это не число! Попробуйте снова')
-            one(x)
+function three(x) {
+    x = Math.floor(Math.random() * 100);
+    let live = 10;
+    function one() {
+        let y = prompt('Напиши твоё число от 1 до 100', 50)
+        function filter() {
+            if (live > 0) {
+                if (false == isNaN(y)) {
+                    num = Number(y)
+                    live--
+                    console.log(live);
+                } else {
+                    alert('Это не число! Попробуйте снова')
+                    one()
+                }
+            } else if (live === 0) {
+                let q = confirm('Ты умер!Играть будешь?')
+                if (false == q) {
+                    alert('Игра окончена!')
+    
+                } else if (true == q) {
+                    one()
+                }
+            }
         }
-    }
-    function two() {
-        if (y == x) {
-            alert('Поздравляю! Ты угадал')
-        } else if (y == null){
-            alert('Игра окончена')
-        } else if (y > x) {
-            alert('Загаданное число меньше!')
-            one(x)
-        } else if (y < x) {
-            alert('Загаданное число больше!')
-            one(x)
+        function two() {
+            if (y == x) {
+                alert('Поздравляю! Ты угадал')
+            } else if (y == null){
+                alert('Игра окончена')
+            } else if (y > x) {
+                alert('Загаданное число меньше!')
+                one(x)
+            } else if (y < x) {
+                alert('Загаданное число больше!')
+                one(x)
+            }
         }
+        filter()
+        two()
     }
-    filter()
-    two()
+    one()
 }
-one(num)
+three(num)
