2cae0073453726d7285052e55ac7d267fe6dd177
Vasily <vasily-bazhenov.03@mail.ru>
2022-03-22 12:42:29 +0300

    js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let text = prompt('skolko', 2000);
-let num;
-const getNum = function(a) {
-    if (false == isNaN(a)) {
-        num = Number(a)
-    } else {
-        text = prompt('skolko', 2000)
-        getNum(text)
+'use strict'
+const arr = ["43523", "3243425", "231234", "435234", "9324213", "23241432", "64352134"]
+
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i].startsWith('2') || arr[i].startsWith('4')){
+        console.log(arr[i]);
     }
+    
 }
-console.log(getNum(text));
-console.log(typeof num);
-console.log(num);
+
+
+let n = 100;
+
+nextPrime:
+for (let i = 2; i <= n; i++) {
+    let j
+  for ( j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime;
+}
+console.log(i, "Делители этого числа: 1 и ", j);
+
+}
