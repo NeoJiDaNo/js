12f740a8035fa401cd4d9f52cb2b0895d86d5d92
Vasily <vasily-bazhenov.03@mail.ru>
2022-03-29 12:51:49 +0300

    js

--- script.js
+++ script.js
@@ -1,109 +1,59 @@
+'use strict';
+
+let book = document.querySelectorAll('.book')
+console.dir(book);
+
+book[1].after(book[0])
+book[4].after(book[3])
+book[4].after(book[2])
+book[3].after(book[2])
+book[2].before(book[5])
 
 
-const appData = {
-    rollback: 10,
-    allServicePrices: 0,
-    fullPrice: 0,
-    servicePercentPrice: 0,
-    title: '',
-    screens: [],
-    screenPrice: 0,
-    adaptive: true,
-    services: {},
-
-    start: function(){
-        appData.asking()
-        appData.addPrice()
-        appData.getFullPrice(appData.screenPrice, appData.allServicePrices)
-        appData.getServicePercentPrices()
-        appData.getTitle() 
-
-        appData.logger()
-    },
+let background = document.querySelector('body')
+background.style.backgroundImage = 'url(./image/you-dont-know-js.jpg)' 
 
 
-    asking: function () {
-        do{
-            appData.title = prompt("Как называется ваш проект?");
-        }while(isNaN(appData.title) === false)
+let book3 = document.querySelectorAll('.book > h2')
 
-    for (let i = 0; i < 2; i++){
-        let name = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные");
-        let price = 0
-
-        do{
-            price = prompt("Сколько будет стоить данная работа?");
-        }while(!appData.isNumber(price))
-
-        appData.screens.push({id: i, name:name, price: price})
-    }
+book3[2].textContent = "Книга 3. this и Прототипы Объектов"
+book3[2].style.color = 'darkkhaki'
 
 
-
-    for (let i = 0; i < 2; i++){
-        let name = prompt("Какой дополнительный тип услуги нужен?");
-        let price = 0;
-
-        do {
-            price = +prompt("Сколько это будет стоить?");
-        } while (!appData.isNumber(price))
-
-        appData.services[i] = [name, +price]
-    }
-
-    appData.adaptive = confirm("Нужен ли адаптив на сайте?");
-    },
-
-    addPrice: function () {
-        for ( let screen of appData.screens){
-            appData.screenPrice += +screen.price
-        }
-        for(let key in appData.services) {
-            appData.allServicePrices += appData.services[key]
-        }
-    },
-
-    isNumber: function (num) {
-        return !isNaN(parseFloat(num)) && isFinite(num)
-    },
-
-    isString: function isString(str) {
-        return (typeof str === "string" || str instanceof String);
-    },
-
-    getRollbackMessage: function(price){
-        if (price >= 30000) {
-            return "Даем скидку в 10%"
-        }else if(price >= 15000 && price< 30000) {
-            return"Даем скидку в 5%"
-        }else if (price >= 0 && price < 15000) {
-            return"Скидка не предусмотрена"
-        }else{
-            return"Что-то пошло не так"
-        }
-    },
-    
-    getFullPrice: function(scr, allServ){
-        appData.fullPrice = scr + allServ;
-    },
-
-    getServicePercentPrices: function(){
-        appData.servicePercentPrice  = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100)) ;
-    },
-
-    getTitle: function() {
-        appData.title =  appData.title.trim()[0].toUpperCase() + appData.title.trim().substr(1).toLowerCase()
-    },
-
-    logger: function(){
-        for (const key in appData) {
-            console.log(appData[key]);
-        }
-        console.log(appData.screens);
-        console.log(appData.services);
-        
-    },
-}
+let adv = document.querySelector('.adv')
+adv.style.display = 'none'
 
 
-appData.start()
+let glav = document.querySelectorAll('.book > ul')
+console.dir(glav)
+
+let glav2 = glav[1].querySelectorAll('li')
+console.log(glav2);
+
+glav2[8].after(glav2[2])
+glav2[6].after(glav2[4])
+glav2[6].after(glav2[5])
+glav2[5].before(glav2[8])
+glav2[5].before(glav2[4])
+glav2[6].after(glav2[4])
+glav2[2].before(glav2[9])
+
+let glav5 = glav[4].querySelectorAll('li')
+console.log(glav5);
+
+glav5[2].before(glav5[9])
+glav5[2].before(glav5[3])
+glav5[2].before(glav5[4])
+glav5[5].before(glav5[6])
+glav5[5].before(glav5[7])
+
+
+// “Глава 8: За пределами ES6”
+let li = document.createElement('li')
+li.innerHTML = '<li>Глава 8: За пределами ES6</li>';
+glav[5].appendChild(li)
+let glav6 = glav[5].querySelectorAll('li')
+console.log(glav6);
+glav6[10].after(glav6[9])
+
+// https://codepen.io/neojidano/pen/PoEjmxv
