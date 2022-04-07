0acc3371c40b3ff062eecfd86e1d12401da2d267
Vasily <vasily-bazhenov.03@mail.ru>
2022-04-07 14:47:10 +0300

    js

--- script.js
+++ script.js
@@ -1,57 +1,16 @@
-let div = document.createElement('div')
-let p = document.createElement('p')
-let body = document.querySelector('body')
-let lft = 0
-let tp = 0
-
-const domElement = function (selector, height, width, bg, fontSize, pos) {
-    if (0 == selector.indexOf('.')) {
-        let str = selector.slice(1)
-        div.classList.add(str)
-        div.textContent = 'gg wp'
-        body.append(div)
-        div.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; font-size: ${fontSize}; position: ${pos}`
-    } else if (0 == selector.indexOf('#')) {
-        let str = selector.slice(1)
-        p.id = str
-        p.textContent = 'gg wp'
-        body.append(p)
-        p.style.cssText = `height: ${height}; width: ${width}; background: ${bg}; font-size: ${fontSize}; position: ${pos}`
-    } else {
-        console.log('Даня гей');
+class First {
+    hello() {
+        console.log("Привет я метод родителя!");
     }
 }
 
-const per = new domElement('.pancgey', '100px', '100px', 'red', '28px', 'absolute')
-
-addEventListener('keydown', function (event) {
-    if (event.keyCode == 38) {
-        tp = tp - 10
-        div.style.top = tp + 'px'
-    } else if (event.keyCode == 39) {
-        lft = lft + 10
-        div.style.left = lft + 'px'
-    } else if (event.keyCode == 40) {
-        tp = tp + 10
-        div.style.top = tp + 'px'
-    } else if (event.keyCode == 37) {
-        lft = lft - 10
-        div.style.left = lft + 'px'
+class Seconds extends First {
+    hello() {
+        super.hello()
+        console.log("А я наследуемый метод!");
     }
-})
+}
 
-addEventListener('keydown', function (event) {
-    if (event.keyCode == 38) {
-        tp = tp - 10
-        p.style.top = tp + 'px'
-    } else if (event.keyCode == 39) {
-        lft = lft + 10
-        p.style.left = lft + 'px'
-    } else if (event.keyCode == 40) {
-        tp = tp + 10
-        p.style.top = tp + 'px'
-    } else if (event.keyCode == 37) {
-        lft = lft - 10
-        p.style.left = lft + 'px'
-    }
-})
+const second = new Seconds
+
+second.hello()
